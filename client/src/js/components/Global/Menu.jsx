2b1cd257f306d7f7db42c943c905c95842faba34
Trung H Tran <tranhoangtrung.nob@gmail.com>
2017-06-08 22:25:37 +0900

    1. Change directory architecture 2. Add same of React Redux 3. No connection with API yet

---
+++ client/src/js/components/Global/Menu.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { IndexLink, Link } from 'react-router';
+import { routeCodes } from '../../routes';
+import signContractImg from '../../../assets/img/sign-contract.svg';
+
+export default class Menu extends Component {
+
+  render() {
+    return (
+      <div className='Menu'>
+        <div className='Menu-logo'>
+          <img
+            src={ signContractImg }
+            alt='sign contract logo'
+          />
+        </div>
+        <div className='Menu-links'>
+          <IndexLink
+            activeClassName='Menu-link--active'
+            className='Menu-link'
+            to={ routeCodes.DASHBOARD }
+          >
+            Home
+          </IndexLink>
+          <Link
+            activeClassName='Menu-link--active'
+            className='Menu-link'
+            to={ routeCodes.ABOUT }
+          >
+            About
+          </Link>
+          <Link
+            activeClassName='Menu-link--active'
+            className='Menu-link'
+            to='404'
+          >
+            404
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ client/src/js/views/About/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className='About'>
+        <h1>About the Project</h1>
+
+        <p>
+          We are building a initial project from scatch using docker!
+        </p>
+        <p>
+        </p>
+      </div>
+    );
+  }
+}
